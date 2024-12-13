612cb409a20007410acd7a855838366594e96ce5
tvkeprof <wefousoul@gmail.com>
2024-12-13 18:05:39 +0800

    part1

--- index.js
+++ index.js
@@ -214,6 +214,16 @@ function namesOnly(arr) {
   To save you from having to count the items above, you can assume that length of originalFlavors is 31. You may also want to test your function by creating and passing an array of a different length, to make sure it returns FALSE if length is not 31.
    
   i.e. is31Flavors(originalFlavors) will return TRUE.*/
+
+  function is31Flavors(arr) {
+    if(arr.length == 31){
+        return true
+    } else {
+        return false
+    }
+  } 
+  console.log(is31Flavors(originalFlavors));
+  
    
   /* Task 2: Corporate has come to you with an idea for a new flavor: Rainbow Sherbert! They think this will be a game changer. You need to modify the array to include this flavor.
    
@@ -228,19 +238,33 @@ function namesOnly(arr) {
    
   For example addFlavor("Rainbow Sherbert", originalFlavors) should return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla Burnt Almond"] */
    
-  function addFlavor(array, flavor) {}
+  function addFlavor(flavor, data) {
+    data.unshift(flavor)
+    return data
+  }
+  console.log(addFlavor("Rainbow Sherbert", originalFlavors))
+  console.log(originalFlavors);
+  
+  
+
+//   console.log(addFlavor("Test", originalFlavors))
+//   console.log(originalFlavors);
+  
    
   /* Task 3: Houston, we have a problem! There are now 32 flavors in the array! Your task is to remove an item from the end of the array.
-   
   Your function should accept:
-   
   (1) an array
-   
   Your function should remove a flavor from the end of the array and console.log the resulting array.
-   
-   
-   
   For example removeLastFlavor(originalFlavors) would return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla"]*/
+
+  function removeLastFlavor(arr) {
+    const removedFlavor = arr.pop();
+    return removedFlavor;
+  } console.log(removeLastFlavor(originalFlavors));
+  console.log(originalFlavors);
+  
+  
+  
    
   /* Task 4: Write a function that returns a flavor at a given index in the array.
    
@@ -250,8 +274,14 @@ function namesOnly(arr) {
   (2) an index
    
   For example, getFlavorByIndex(originalFlavors, 2) would return "Black Walnut", assuming Rainbow Sherbert has been added successfully. */
+
+  function getFlavorByIndex(arr, index) {
+    return arr[index - 1]
+  } console.log(getFlavorByIndex(originalFlavors, 8));
+  
    
-  /* Task 5: As corporate wants to add more and more flavors to their lineup, they've realized that they need to remove flavors based on flavor name, as opposed to just arbitrarily removing the first or last flavor. Your task is to get an index by flavor name, and remove that flavor from the array.
+  /* Task 5: As corporate wants to add more and more flavors to their lineup, they've realized that they need to remove flavors based on flavor name,
+   as opposed to just arbitrarily removing the first or last flavor. Your task is to get an index by flavor name, and remove that flavor from the array.
    
   Your function should accept:
    
@@ -263,6 +293,16 @@ function namesOnly(arr) {
   Hint: You can use .splice() for this
    
   */
+ function removeFlavorByName(arr, flavorName) {
+    const index= arr.indexOf(flavorName)
+    if (index !== -1) {
+        arr.splice(index, 1);
+    } return flavorName
+ } 
+ console.log(removeFlavorByName(originalFlavors, "Vanilla"));
+ console.log(originalFlavors);
+ 
+ 
    
   /* Task 6: With all of these changes going on, we don't want to lose track of the actual, original 31 flavors. Write a function called copy that makes a copy of the array.
    
@@ -271,8 +311,18 @@ function namesOnly(arr) {
   2 arguments 1 for your new array and one for your original array
    
   and should return a new array that is identical to the old array. You can name the new array however you'd like. */
+  function copy(arr) {
+    return[...arr];
+  } const copiedFlavors = copy(originalFlavors)
+
+  console.log(copiedFlavors);
+  
    
-  /* Task 7: July 7th is "World Chocolate Day" and Baskin Robins wants to create promotional materials highlighting all of their chocolate flavors. Write a function that checks every item in the array for a given string and returns a new array called filteredArray with just these values. Rather than hardcoding "chocolate" into your function, pass a string as a parameter, and invoke with the argument "chocolate". This way you could also filter for "Vanilla", "Sherbert", etc. when those holidays roll around.
+  /* Task 7: July 7th is "World Chocolate Day" and Baskin Robins wants to create promotional materials highlighting all of their 
+  chocolate flavors.
+   Write a function that checks every item in the array for a given string and returns a new array called filteredArray with just these values. 
+   Rather than hardcoding "chocolate" into your function, pass a string as a parameter, and invoke with the argument "chocolate". 
+   This way you could also filter for "Vanilla", "Sherbert", etc. when those holidays roll around.
    
   Your function should accept:
    
@@ -286,18 +336,23 @@ function namesOnly(arr) {
   DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem.
    
   hint - you can use the .includes method to help you solve this */
+  function filterByWord(arr, string) {
+    return arr.filter(flavor => flavor.includes(string));
+  }
+  console.log(filterByWord(originalFlavors, "Chocolate"));
+  
    
-  // function filterByWord(array, word) {
-  //   const filteredArray = [];
-  //   array.forEach(function (flavor) {
-  //     if (flavor.includes(word)) {
-  //       console.log(flavor);
-  //     }
-  //   });
-  //   //   return filteredArray;
-  // }
+//   function filterByWord(array, word) {
+//     const filteredArray = [];
+//     array.forEach(function (flavor) {
+//       if (flavor.includes(word)) {
+//         console.log(flavor);
+//       }
+//     });
+//       return filteredArray;
+//   }
    
-  // console.log(filterByWord(originalFlavors, "chocolate"));
+//   console.log(filterByWord(originalFlavors, "chocolate"));
   /* 🧁🍦🍨 STRETCH 🍨🍦🍫*/
    
   /* STRETCH 1: Write a function that returns the average number of words in an array. You should be able to use this function for any array, but can test with originalFlavors.
@@ -310,6 +365,94 @@ function namesOnly(arr) {
    
   For example, getAverageWordLength(originalFlavors) should return a number between 0 and 3. */
    
-  function getAverageWordLength(/*code here*/) {
-    /*code here*/
-  }
+  function getAverageWordLength(arr) {
+    let total=0
+    for (let i = 0; i < arr.length; i++) {
+        total += arr[i].split(' ').length;
+        
+    } return total / arr.length
+  } console.log(getAverageWordLength(originalFlavors)); 
+  
+
+
+
+ // Array exercises 2
+// Exercise 1
+// Write a JavaScript function to sort three numbers in array. Return sorted array.
+// Sample numbers : [0, -1, 4]
+// Output : [4, 0, -1]
+// notes:
+//     https://www.geeksforgeeks.org/javascript-array-sort/
+//     https://www.w3schools.com/jsref/met_win_alert.asp
+ 
+// 1. write function ==> function will take array as input
+// 2. sort the numbers inside the array and return sorted array
+ 
+function sortedArray(array) {}
+ 
+console.log(sortedArray([5, 7, 10, -10, 56]));
+// ==> [56, 10, 7,5,-10]
+/*
+ 
+ 
+ 
+Exercise 1
+ 
+Create a function that will display the smallest value in the array.
+ 
+Example:
+> console.log(findSmallest([30, 45, 60, 7]));
+> 1
+ 
+ 
+Exercise 2
+ 
+Create a function that will accept an array, check the data type of each element. The function will delete string elements and will return a the new array
+ 
+Example:
+> console.log(numbersOnly(['text', 3, 7, 'github', 13, 'dev']));
+> [ 3, 7, 13 ]
+ 
+Exercise 3
+ 
+Create a function that will accept an array and do the following:
+Get the lowest element
+Get the highest element
+Get the length of array
+Get the Average of all element;
+Store these criteria in a new array
+ 
+Example:
+> console.log(minMaxLengthAverage([7, 13, 3, 77, 100]));
+> [ 3, 100, 5, 40 ]
+ 
+Exercise 4
+ 
+Count the number of Words
+Return how many words was given
+ 
+Example:
+> countWords('hello from kbpsystem!');
+> 3
+ 
+Exercise 5
+ 
+Multiply by Length
+Multiply all elements in an array by it's length
+ 
+Example:
+> MultiplyByLength([4,1,1]);
+> [12, 3, 3]
+ 
+Exercise 6
+ 
+Find the correct Index location
+Return the index location of an element from a given array. First argument is the array you'd like to search and the second one is the element (either string/number) to look for.
+ 
+Example:
+> console.log(findIndex(['github', 'gitlab', 'bitbucket', 'apollo'], 'gitlab'));
+> 1
+ 
+*/
+ 
+ 
